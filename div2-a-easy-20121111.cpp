e121fa2577c0ec4a0759992f57a7acb5f94938c8
kyos <kyusyukyos@yahoo.co.jp>
2012-11-11 06:32:10 +0900

    SRM560

---
+++ div2-a-easy-20121111.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+class TypingDistance{
+private:
+	int key_pos[26];
+	
+public:
+	int minDistance(string keyboard, string word){
+		int ans=0;
+		
+		for(int i=0;i<keyboard.size();i++){
+			key_pos[keyboard[i]-'a']=i;
+		}
+		for(int i=1;i<word.size();i++){
+			ans=ans+abs(key_pos[word[i-1]-'a']-key_pos[word[i]-'a']);
+			cout<<ans<<endl;
+		}
+		
+		return ans;
+	}
+};
+
+
+
+
+int main(){}
